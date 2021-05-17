9cad814ed01cb4bb8ce7ddf469eef4548fbc884c
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-05-17 20:28:39 +0300

    Resolve a namespace and type conflict using global alias

---
+++ Namespaces/Namespaces.Main/ConsoleHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Namespaces.Main
+{
+    namespace System.Console
+    {
+        public class ConsoleHelper
+        {
+            public static void DoWork()
+            {
+                global::System.Console.WriteLine("Doing work");
+            }
+        }
+    }
+    
+}
